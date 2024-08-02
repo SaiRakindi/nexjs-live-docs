6b080ee2643e0a501f721e3f7fcf027d5feabe4b
SaiRakindi <sai.rakindi3@gmail.com>
2024-08-02 20:43:21 +0530

    feat: header sign in and sign up pages

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,5 +1,13 @@
 import { Editor } from "@/components/editor/Editor";
 import Header from "@/components/ui/Header";
+import {
+  SignedIn,
+  SignedOut,
+  SignIn,
+  SignInButton,
+  SignOutButton,
+  UserButton,
+} from "@clerk/nextjs";
 import React from "react";
 
 const Document = () => {
@@ -9,6 +17,14 @@ const Document = () => {
         <div className="flex w-fit items-center justify-center gap-2">
           <p className="document-title">This is a fake document title</p>
         </div>
+
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </Header>
       <Editor />
     </div>
